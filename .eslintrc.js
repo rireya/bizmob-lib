f98be27bbcf6953153c18bdae83e8fbc14253067
rireya <rireya@outlook.com>
2025-07-22 17:30:19 +0900

    ESLint 설정 업데이트: TypeScript 관련 규칙 조정 및 프로덕션 환경에 따른 console과 debugger 사용 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,36 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 'warn',
     'no-console': 'warn',
     'no-debugger': 'error',
+
+    // TypeScript의 any 타입 사용을 금지하지 않음.
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    // TypeScript의 사용하지 않는 변수 오류 off
+    '@typescript-eslint/no-unused-vars': 'off',
+
+    // no-undef 규칙 비활성화 (TypeScript가 처리)
+    'no-undef': 'off',
+
+    // 프로덕션 환경에서는 console 사용에 대해 경고를 표시하고, 그 외 환경에서는 금지하지 않음.
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    // 프로덕션 환경에서 debugger 사용에 대해 경고를 표시하고, 그 외 환경에서는 금지하지 않음.
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    // 문단 마지막 항상 세미콜론
+    'semi': ['error', 'always'],
+
+    // single quote 사용
+    'quotes': ['error', 'single'],
+
+    // Tab은 Space 2칸으로 정의
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+
+    // var 선언 금지 off
+    'no-var': 'off',
+
+    // arguments 객체 사용 금지 off
+    'prefer-rest-params': 'off',
   },
   overrides: [
     {
